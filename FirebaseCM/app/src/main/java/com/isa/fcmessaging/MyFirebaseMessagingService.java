13f92a82df39dbf31226c4a220984e4e67b24f86
Isabel Contreras <isabel.contreras.1999@hotmail.com>
2021-06-06 04:46:01 -0500

    Integración de proyecto Cliente y Servidor de FCM.

---
+++ FirebaseCM/app/src/main/java/com/isa/fcmessaging/MyFirebaseMessagingService.java
@@ -0,0 +1,44 @@
+package com.isa.fcmessaging;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private final String TAG = MainActivity.TAG;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "Message From: " + remoteMessage.getFrom());
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage
+                        .getNotification().getBody());
+        }
+    }
+
+    /**
+     * There are two scenarios when onNewToken is called:
+     * 1) When a new token is generated on initial app startup
+     * 2) Whenever an existing token is changed
+     * Under #2, there are three scenarios when the existing token is changed:
+     * A) App is restored to a new device
+     * B) User uninstalls/reinstalls the app
+     * C) User clears app data
+     */
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        //sendRegistrationToServer(token);
+    }
+}
---
+++ Servidor/ServidorFCM/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+
+namespace ServidorFCM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Inicialización del SDK
+            FirebaseApp.Create(new AppOptions() {
+                Credential = GoogleCredential.FromFile("C:/Users/Lenovo T540P/OneDrive/Documentos/ITSUR/VIII Semestre/MOVIL II - 2/U6/Firebase/Servidor/fcmessaging-18e6c-firebase-adminsdk-wgy3l-efdb41a902.json")
+                //Storage = StorageClient.Create(credential)
+                //Credential = GoogleCredential.GetApplicationDefault(),
+            });
+
+            //autenticar con un token de actualización de Google OAuth2
+            /*
+            FirebaseApp.Create(new AppOptions(){
+                Credential = GoogleCredential.FromFile("path/to/refreshToken.json"),
+            });*/
+
+            // This registration token comes from the client FCM SDKs.
+            //Task t = EnviarMsj();
+            //t.Wait();
+            Task t = EnviarNotificacion();
+            t.Wait();
+
+        }
+
+        static async Task EnviarMsj()
+        {
+            var registrationToken = "dnvCJQ9CQA-owE-W19PH1h:APA91bEcKTScvzPXoBqFcJihNHGwNpwzpoyhaSxzV6GYYmTcSEdAWoJ-tL_wCjUE9_K_xQp8tUcfq4fqxF4rAv51WolNKgAHH_6D4aNZTWrxZPRiE2vlUv1do3-Ney3uXUoQQ38YOnbo";
+
+            // See documentation on defining a message payload.
+            var message = new Message()
+            {
+                Data = new Dictionary<string, string>()
+                {
+                    { "score", "120" },
+                    { "time", "3:45" },
+                },
+                Token = registrationToken,
+            };
+
+            // Send a message to the device corresponding to the provided
+            // registration token.
+            string response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+            // Response is a message ID string.
+            Console.WriteLine("Successfully sent message: " + response);
+        }
+
+        static async Task EnviarNotificacion(){
+            var registrationToken = "eR7w091tRMaVGVUaxmx3qB:APA91bEZWd95tS7hOGuEdX3FcryNq7UN_z1IZHTl46owFrYZB-vOtIcFaykAh-Tj9v9EA3qGEsSiYdz1V26mZKdSd6Dbq5ulnl6ynyERs7649Xce-sEaN28QAGg9STrRO6tXdaxVy0Dd";
+            var message = new Message{
+                Notification = new Notification()
+                {
+                    Title = "Canción recomendada del día",
+                    Body = "The Weeknd - Save Your Tears",
+                },
+                Android = new AndroidConfig()
+                {
+                    TimeToLive = TimeSpan.FromHours(1),
+                    Notification = new AndroidNotification()
+                    {
+                        Icon = "stock_ticker_update",
+                        Color = "#BD1258",
+                    },
+                },
+                Token = registrationToken,
+            };
+            string response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+            // Response is a message ID string.
+            Console.WriteLine("Successfully sent message: " + response);
+        }
+
+
+    }
+}
